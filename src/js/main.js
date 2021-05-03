ff32a83e0a2321a9607be5a2928d7b1c29d8ea08
NathanZog <81653297+NathanZog@users.noreply.github.com>
2021-05-04 00:52:25 +0200

    devoir exo array 6

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+// 1 !
+let monTab =["crabe", "crevette", "homard", "cloporte"];
+monTab.pop();
+monTab.shift();
+monTab.push("mégalodon");
+
+monTab.unshift("ptéranondon");
+
+monTab.splice(1,1,monTab[1].toUpperCase());
+console.log(monTab);
+console.log(monTab.reverse());
+
+// 2 !!
+let chiffres = [4, 5, 2, 1, 3];
+chiffres.sort();
+let chiffres2 = chiffres.join();
+console.log(chiffres);
+chiffres = chiffres2.split();
+
+//3
+
+let tab = [];
+tab.push("elem1","elem2","elem3","elem4","elem5","elem6","elem7","elem8","elem9","elem10");
+console.log(tab);
+let item = tab.indexOf("elem7");
+console.log(item);
+tab.splice(5,1);
+console.log(tab);
+
+let tabclasse = [];
+tabclasse.push("Mouna","Ilias El", "Agim", "Kevin", "Antoine", "Seifidin", "Chris", "Haroune", "Elvis", "Nasila", "Van Hoa", "Jean", "Stan", "Ilias D", "Nathan", "Adil", "Ali", "Damien", "Yassine", "Lira");
+let random = Math.floor(Math.random() * tabclasse.length);
+console.log(tabclasse[random]);
+tabclasse.splice(14,1)
+console.log(tabclasse)
